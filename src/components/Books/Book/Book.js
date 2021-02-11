a43bf821835e50ce5fb390b50ab40194afdb15cd
Ananya Kaushik <home.ananya@gmail.com>
2021-02-10 23:24:31 -0800

    give Books page more consistent styling

--- src/components/Books/Book/Book.js
+++ src/components/Books/Book/Book.js
@@ -7,24 +7,26 @@ import classes from './Book.module.css';
 // Component for each individual Book
 const Book = (props) => {
     
-    // alternate styling of the book's description (with its alternate positioning)
-    const bookDescStyle = (props.id % 2 !== 0) ? classes.BookDescLeft : classes.BookDescRight;
+    // alternate styling float direction of the book's description and image
+    const bookDescFloat = (props.id % 2 === 0) ? "float-md-right" : "float-md-left";
+    const imgFloat = (props.id % 2 === 0) ? "float-md-left" : "float-md-right";
 
     return(
         <Row className={classes.BookDiv}>
-            {/* alternate the position of the book's image */}
-            {props.id % 2 === 0 && <BookImage />}
 
-            <BookDesc
-                className={bookDescStyle}
-                title={props.title}
-                date={props.date}
-                author={props.author}
-                genre={props.genre}
-                rating={props.rating}
-                description={props.description} />
-                
-            {props.id % 2 !== 0 && <BookImage />}
+            {/* alternate the position of the book's image */}
+            <div>
+                <BookImage float={imgFloat}/>
+                <BookDesc
+                    float={bookDescFloat}
+                    title={props.title}
+                    date={props.date}
+                    author={props.author}
+                    genre={props.genre}
+                    rating={props.rating}
+                    description={props.description} />
+            </div>
+            {/* <div className="w-100">{props.id < 4 && <hr className={classes.hr} />}</div> */}
         </Row>
     )
 }
--- src/components/Books/Book/BookImage.js
+++ src/components/Books/Book/BookImage.js
@@ -6,7 +6,7 @@ import classes from './Book.module.css';
 // Component for the book's image
 const BookImage = (props) => {
     return(
-        <Col className={classes.BookImg} lg={4} md={4} >
+        <Col className={`${classes.BookImg} ${props.float}`} lg={4} md={4} >
             <img src={plc} alt="Name of Book" />
         </Col>
     );
